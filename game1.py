8d2613852a78b0996d8cda1f77fd1b1e45e3d97d
Alexander1022 <36764735+Alexander1022@users.noreply.github.com>
2018-03-16 18:50:59 +0200

    Add files via upload

---
+++ game1.py
@@ -0,0 +1,157 @@
+#! python
+import pyglet
+music = pyglet.resource.media('sound.mp3')
+music.play()
+import sys
+import time
+print("Time Machine 0.0.1 beta")
+yesNo = input("Do you want to start(Y or N): ").upper()
+if yesNo == "Y":
+    print("LOADING")
+else:
+    print("YOU ARE NOT TOO COOL FOR US")
+    exit()
+
+
+for i in "........................":
+    print(i,end='')
+    time.sleep(0.2)
+print()
+name=input("WRITE YOUR NAME:")
+print("HI",name)
+for i in "-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-":
+    print(i,end='')
+    time.sleep(0.01)
+print()
+input("press ENTER!")
+print("YOU WALK THORUGH THE FOREST AND FIND THE TIME MACHINE")
+for i in "▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄ ▀ ▄":
+    print(i,end='')
+    time.sleep(0.02)
+print("")
+for c in "DO YOU WANT TO JOIN IT??!":
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+a=input("y OR n:");
+if a == ("y"):
+    print("GREAT!!!")
+else:
+    print("GOODBYE AMIGOSS")
+    exit()
+
+for c in "WE ARE GOING TO THE PAST -- WORLD WAR 1 ":
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+print("")
+for i in "UNFORTUNATELY THE TIME MACHINE IS BROKEN":
+    print(i,end='')
+    time.sleep(0.2)
+print()
+
+for c in "NOW,YOU MUST PLAY IT, ":
+    sys.stdout.write(c)
+    sys.stdout.flush()  
+    time.sleep(0.1)
+print("THAT IS GOING TO BE INTERESTING!!! :)")
+
+print("YOU HAVE LIFE AND SHIELD")
+for c in "YOU MUST CHOOSE  ":
+    sys.stdout.write(c)
+    sys.stdout.flush()  
+    time.sleep(0.1)
+action=input("shield OR helmet:");
+if action == ("shield"):
+    print("YOOO BRO,YOU HAVE PROTECTION")
+elif action=="helmet":
+    print("YOUR HEAD IS SO STROOONG")
+
+else:
+    print("YOU ARE DEAD!!!")
+    exit()
+ 
+input("PRESS ENTER TO JOIN THE WAR")
+print("There are some soldiers,",name)
+for c in "THEY ARE GOING TO FIND A GUN FOR YOU,MAYBE A SNIPER RIFLE?!?":
+    sys.stdout.write(c)
+    sys.stdout.flush()  
+    time.sleep(0.1)
+print("")  
+string = """
+⌐╦╦═─   .
+
+⌐╦╦═─        .
+
+⌐╦╦═─             .
+
+⌐╦╦═─                  .
+
+⌐╦╦═─                      .
+
+⌐╦╦═─                          .
+
+⌐╦╦═─                             .
+
+⌐╦╦═─                                .
+
+⌐╦╦═─                                   .
+                                                                          
+"""
+print("EXCELLENT,YOU MADE IT")
+for c in "YOU KILLED THEM ALMOST":
+    sys.stdout.write(c)
+    sys.stdout.flush()  
+    time.sleep(0.1)
+print("")
+for line in string.splitlines():
+    print(line)
+    time.sleep(0.08)
+
+print("")
+print("GOOD,BRO")
+for c in "BUT THE MACHINE IS FIXED":
+    sys.stdout.write(c)
+    sys.stdout.flush()  
+    time.sleep(0.1)
+
+
+for c in " FOR NOW,THAT IS OKEY ;) ":
+    sys.stdout.write(c)
+    sys.stdout.flush()  
+    time.sleep(0.1)
+print("")
+b=input("Do you want to meet Napoleon??!?");
+if b==("yes"):
+    print("YEAH,BOIIII")
+else:
+    print("oh")
+    exit()
+for i in "SO WHAT YO GONNA DO WITH NAPOLEON":
+    print(i,end='')
+    time.sleep(0.2)
+print()
+q=input("FIGHT or BE A weaken")
+if q == ("fight"):
+    print("YOU FIGHT IT")
+elif q ==("be a weaken"):
+    print("dont be stupid!!!!")
+
+print("NAPOLEON IS RUNNING AFTER YOU")
+    
+
+
+
+
+
+
+
+
+                             
+
+
+
+
+
